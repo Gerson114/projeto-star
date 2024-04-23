5d1370e1d88ddb48daf8e52ef0e579305f4cff5c
Gerson114 <164920308+Gerson114@users.noreply.github.com>
2024-04-23 16:43:27 -0300

    menu

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -7,28 +7,26 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <header>
+      <div className='fundo'>
+      <div className='logo'><h1><strong>Mercado</strong></h1></div>
+      <div className='menu'>
+          <nav>
+             <a href="#">home</a>
+             <a href="#">contato</a>
+             <a href="#">sobre nois</a>
+             <a className='conta' href="#">Criar conta</a>
+          </nav>
+        </div>
+          <div className='social'>
+            <button>entrar</button>
+
+          </div>
+       </div> 
+
+
+
+    </header>
   )
 }
 
