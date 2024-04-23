70fb297fefd6cd72efe2390568e5308c874bef97
Gerson114 <164920308+Gerson114@users.noreply.github.com>
2024-04-23 18:10:40 -0300

    menu atualizado

--- start/src/App.jsx
+++ start/src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import menuu from './componets/menu';
 import './App.css'
+
+
+            //------menu------//
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <header>
-      <div className='fundo'>
-      <div className='logo'><h1><strong>Mercado</strong></h1></div>
-      <div className='menu'>
-          <nav>
-             <a href="#">home</a>
-             <a href="#">contato</a>
-             <a href="#">sobre nois</a>
-             <a className='conta' href="#">Criar conta</a>
-          </nav>
-        </div>
-          <div className='social'>
-            <button>entrar</button>
-
-          </div>
-       </div> 
-
-
-
-    </header>
+    
+    <menuu menuu={menuu}/>
+  
+  
+  
+  
   )
 }
 
-export default App
+export default menuu
--- start/src/componets/menu.jsx
+++ start/src/componets/menu.jsx
@@ -1,1 +1,27 @@
+ import React from 'react'
  
+ const menu = ({menuu}) => {
+   return (
+    <header>
+    <div className='fundo'>
+    <div className='logo'><h1><strong>Mercado</strong></h1></div>
+    <div className='menu'>
+        <nav>
+           <a href="#">home</a>
+           <a href="#">contato</a>
+           <a href="#">sobre nois</a>
+           <a className='conta' href="#">Criar conta</a>
+        </nav>
+      </div>
+        <div className='social'>
+          <button>entrar</button>
+
+        </div>
+     </div> 
+      
+
+  </header>
+   )
+ }
+ 
+ export default menu
